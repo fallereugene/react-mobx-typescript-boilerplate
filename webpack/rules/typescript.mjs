e6ade045fa710823bb568e918926e4096aeeaec8
Eugene <faller.eugene@gmail.com>
2023-10-11 13:47:11 +0300

    build: изменение системы сборки
    Изменения правил для typescript файлов, оптимизации чанков, core-js убран из входной точки.

--- webpack/rules/typescript.mjs
+++ webpack/rules/typescript.mjs
@@ -1,12 +1,28 @@
+import { join } from 'path';
+import { rootDir, excludeNodeModulesExcept } from '../utils/index.mjs';
+
 export const typescriptRule = {
     test: /\.tsx?$/,
-    loader: 'ts-loader',
-    options: {
-        // disable type checker - we will use it in fork plugin
-        transpileOnly: true,
-        logLevel: `info`,
-        configFile: 'tsconfig.json',
-        happyPackMode: true,
-    },
-    exclude: /node_modules/,
+    use: [
+        {
+            loader: 'babel-loader',
+            options: {
+                configFile: join(rootDir, '/.babelrc.js'),
+                cacheCompression: false,
+                cacheDirectory: true,
+            },
+        },
+        {
+            loader: 'ts-loader',
+            options: {
+                // disable type checker - we will use it in fork plugin
+                transpileOnly: true,
+                logLevel: `info`,
+                configFile: 'tsconfig.json',
+                happyPackMode: true,
+            },
+        },
+    ],
+
+    exclude: excludeNodeModulesExcept([]),
 };
