58b4bd489aad942985046a163a16675fdeb45a54
Eugene <faller.eugene@gmail.com>
2024-04-10 19:29:06 +0300

    fix: при обновлении сервис-воркера не происходит перерегистрация
    Также исправлена ошибка удаления зарегистрированного сервис-воркера.

--- src/containers/layout/styles.ts
+++ src/containers/layout/styles.ts
@@ -1,5 +1,4 @@
 import { Interpolation, Theme } from '@mui/material/styles';
-import { SxProps } from '@/contracts/theme';
 
 export const globalStyles: Interpolation<Theme> = {
     a: {
@@ -8,30 +7,5 @@ export const globalStyles: Interpolation<Theme> = {
     },
     '#root': {
         padding: '0 15px',
-    },
-};
-
-export const rootStyles: SxProps = {
-    display: 'flex',
-    maxWidth: 1920,
-    margin: 'auto',
-    '& .main.MuiBox-root': {
-        flexGrow: 1,
-        p: 3,
-        padding: '50px 75px',
-        position: 'relative',
-        minHeight: '100vh',
-    },
-    '& .notify-stack': {
-        position: 'fixed',
-        top: '50px',
-        right: '50px',
-        zIndex: '10000 !important',
-        '& .MuiSnackbar-root': {
-            minWidth: '475px',
-            display: 'block',
-            position: 'relative',
-            marginBottom: '10px',
-        },
     },
 };
--- src/routes/main/components/filter/index.tsx
+++ src/routes/main/components/filter/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useTheme } from '@mui/material';
 import cn from 'classnames';
 import MUIButtonGroup from '@mui/material/ButtonGroup';
 import { Button } from '@components/form';
 import { useI18n } from '@/hooks';
+import { SxProps } from '@/contracts/theme';
 import { AVAILABLE_FILTER_VALUES } from '../../constants';
-import { styles } from './styles';
+
+export const styles: SxProps = (theme) => ({
+    margin: '20px 0',
+    '& .MuiButtonBase-root': {
+        '&.active': {
+            cursor: 'default',
+            opacity: 0.5,
+        },
+    },
+    [theme.breakpoints.down('sm')]: {
+        width: '100%',
+        '& .MuiButtonBase-root': {
+            width: '100%',
+        },
+    },
+});
 
 type FilterProps = {
     activeFilter: string;
@@ -14,8 +31,11 @@ type FilterProps = {
 
 export const Filter: React.FunctionComponent<FilterProps> = ({ activeFilter, values, onFilterClick }) => {
     const { t } = useI18n();
+    const theme = useTheme();
+    const classes = styles(theme);
+
     return (
-        <MUIButtonGroup variant="contained" aria-label="Basic button group" sx={styles}>
+        <MUIButtonGroup variant="contained" aria-label="Basic button group" sx={classes}>
             {values.map((item) => (
                 <Button
                     key={item}
